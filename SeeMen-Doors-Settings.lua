8c170b3c80f9a4809f307d40fb2cffb3b25f650d
Ben Rozhon <140898338+Ben-Rozhon@users.noreply.github.com>
2024-12-20 17:10:30 -0600

    Add files via upload

---
+++ SeeMen-Doors-Settings.lua
@@ -0,0 +1,68 @@
+getgenv().SeeMen = {
+    Doors = {
+        Main = {
+            notifications = false,
+            motificationSound = false,
+            notificationSoundVolume = 1,
+            notifyAnchorCode = false,
+            notifyPadlockCode = false,
+            notifyEntities = false,
+            notifyEntitiesList = {},
+        },
+
+        Exploits = {
+            Self= {
+                allowJump = false,
+                antiGloombat = false,
+                antiGiggle = false,
+                antiSnare = false,
+                antiDupe = false,
+                antiEyes = false,
+                antiLookman = false,
+                antiChandelier = false,
+                antiSeekArms = false
+            },
+            Bypass = {
+                crouchSpoof = false,
+                speedBypass = false,
+                ACManipulation = false
+            },
+            Removals = {
+                removeSeek = false,
+                removeScreech = false,
+                removeA90 = false,
+                removeShade = false,
+                removeA90Damage = false,
+                removeScreechDamage = false,
+                removeShadeDamahe = false
+            },
+            Troll = {
+                spamTools = false,
+                whitelist = {}
+            }
+        },
+
+        Automation = {
+            autoInteract = false,
+            autoInteractRange = 1,
+            autoPadlockSolver = false,
+            autoPadlockSolverRange = 1,
+            autoMinecartInteract = false,
+            autoAnchorSolver = false
+        },
+
+        Visual = {
+            espPlayer = false,
+            espPlayerFillColor = Color3.fromRGB(1, 1, 1),
+            espPlayerOutlineColor = Color3.fromRGB(1, 1, 1),
+            espInteractables = false,
+            espFont = Enum.Font.RobotoMono,
+            espFontSize = 20,
+            espFadeTime = 1,
+        },
+
+        Misc = {
+            
+        }
+    }
+}
